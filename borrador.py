8bfcb6d5c02dce1e7d1257b054a5fab04000ac1e
Leo <bordonleonel977@gmail.com>
2025-06-08 17:23:46 -0300

    Implementar operacion POST para crear mensajes

---
+++ borrador.py
@@ -0,0 +1,5 @@
+@app.post("/mensajes", response_model=Mensaje)
+def crear_mensaje(mensaje: Mensaje):
+    mensaje.id = len(mensajes_db) + 1
+    mensajes_db.append(mensaje)
+    return mensaje
---
+++ main.py
@@ -0,0 +1,21 @@
+from typing import Optional, List
+from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+
+# Modelo del mensaje
+class Mensaje(BaseModel):
+    id: Optional[int] = None
+    user: str
+    mensaje: str
+
+# Crear instancia de la aplicación FastAPI
+app = FastAPI()
+
+# Base de datos simulada (lista en memoria)
+mensajes_db: List[Mensaje] = []
+
+@app.post("/mensajes", response_model=Mensaje)
+def crear_mensaje(mensaje: Mensaje):
+    mensaje.id = len(mensajes_db) + 1
+    mensajes_db.append(mensaje)
+    return mensaje
